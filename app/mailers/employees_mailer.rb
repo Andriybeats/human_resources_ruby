f4878d81658d035fff0388a7fd1138b83f048324
Andriybeats <nabivanec.a@gmail.com>
2019-04-12 23:27:36 +0300

    fixed , int employees mailer

--- app/mailers/employees_mailer.rb
+++ app/mailers/employees_mailer.rb
@@ -1,8 +1,10 @@
+=begin
 class EmployeeMailer < ApplicationMailer
-  default (:from => "nabivanec.a@gmail,com", :to=>"nabivanec.a@gmail,com"), template_path: 'mailers/items'
+  default (:from => "nabivanec.a@gmail,com", :to=>"nabivanec.a@gmail.com"), template_path: 'mailers/items'
       def employee_fired(notification)
         @notification = notification
         @url  = "nabivanec.a@gmail,com"
         mail(:subject => "New Notification")
       end
 end
+=end
